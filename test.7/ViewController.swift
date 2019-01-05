59f92fc95f212a11910b432694b25d72717573da
cy1997cj <1418296577@qq.com>
2019-01-05 10:01:36 +0800

    计算器999
    计算器999

--- test.7/ViewController.swift
+++ test.7/ViewController.swift
@@ -13,52 +13,101 @@ class ViewController: UIViewController {
     var operatorFlag = 0
     var performOperation = 0
     var temp1:Double = 1
+    var re = 0
+    var plus = 0
+    var judge = 0
+    
 
     @IBOutlet var result: UITextField!
     
     
     @IBAction func button1(_ sender: Any) {
+        if re == 1{
+            result.text = "1"
+        }else{
         result.text = result.text! + "1"
+        }
     }
     
     @IBAction func button2(_ sender: Any) {
-        result.text = result.text! + "2"
+        if re == 1{
+            result.text = "2"
+        }else{
+            result.text = result.text! + "2"
+        }
     }
     
     @IBAction func button3(_ sender: Any) {
-        result.text = result.text! + "3"
+        if re == 1{
+            result.text = "3"
+        }else{
+            result.text = result.text! + "3"
+        }
     }
     
     @IBAction func button4(_ sender: Any) {
-        result.text = result.text! + "4"
+        
+        if re == 1{
+            result.text = "4"
+        }else{
+            result.text = result.text! + "4"
+        }
     }
     
     @IBAction func button5(_ sender: Any) {
-        result.text = result.text! + "5"
+        if re == 1{
+            result.text = "5"
+        }else{
+            result.text = result.text! + "5"
+        }
     }
     
     @IBAction func button6(_ sender: Any) {
-        result.text = result.text! + "6"
+        if re == 1{
+            result.text = "6"
+        }else{
+            result.text = result.text! + "6"
+        }
     }
     
     @IBAction func button7(_ sender: Any) {
-        result.text = result.text! + "7"
+        if re == 1{
+            result.text = "7"
+        }else{
+            result.text = result.text! + "7"
+        }
     }
     
     @IBAction func button8(_ sender: Any) {
-        result.text = result.text! + "8"
+        if re == 1{
+            result.text = "8"
+        }else{
+            result.text = result.text! + "8"
+        }
     }
     
     @IBAction func button9(_ sender: Any) {
-        result.text = result.text! + "9"
+        if re == 1{
+            result.text = "9"
+        }else{
+            result.text = result.text! + "9"
+        }
     }
     
     @IBAction func button0(_ sender: Any) {
-        result.text = result.text! + "0"
+        if re == 1{
+            result.text = "0"
+        }else{
+            result.text = result.text! + "0"
+        }
     }
     
     @IBAction func button(_ sender: Any) {
-        result.text = result.text! + "."
+        if re == 1{
+            result.text = "."
+        }else{
+            result.text = result.text! + "."
+        }
     }
     
     
@@ -66,9 +115,8 @@ class ViewController: UIViewController {
     
     @IBAction func buttonadd(_ sender: Any) {
         operatorFlag = 1
-            temp = temp+Double(result.text!)!
-            result.text = "+"
- 
+        temp = temp+Double(result.text!)!
+        result.text = "+"
     }
     
     @IBAction func buttonsub(_ sender: Any) {
@@ -119,6 +167,7 @@ class ViewController: UIViewController {
         {
         temp = temp + Double(result.text!)!
         result.text = "\(temp)"
+            temp1=1
         }
         
         
@@ -143,11 +192,16 @@ class ViewController: UIViewController {
             temp = temp / Double(result.text!)!
             result.text = "\(temp)"
             temp1=0
+            
         }
         
         temp=1
+        
+        
     }
     
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
